994f4351a82c6cad98f19d44181587fe275c39e2
Java Developer <developer@example.com>
2026-08-05 17:40:28 +0530

    Remove org.jspecify annotation from CustomUserDetails for clean Spring Boot 3.3.2 build

--- auth-service/src/main/java/com/auth/auth_service/security/CustomUserDetails.java
+++ auth-service/src/main/java/com/auth/auth_service/security/CustomUserDetails.java
@@ -1,15 +1,12 @@
 package com.auth.auth_service.security;
 
 import com.auth.auth_service.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
-
 
 public class CustomUserDetails implements UserDetails {
 
@@ -22,11 +19,11 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         System.out.println("Role from DB: " + user.getRole());
-        return List.of(new SimpleGrantedAuthority("ROLE_"+user.getRole().name()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
     }
 
     @Override
-    public @Nullable String getPassword() {
+    public String getPassword() {
         return user.getPassword();
     }
 
@@ -34,6 +31,7 @@ public class CustomUserDetails implements UserDetails {
     public String getUsername() {
         return user.getUsername();
     }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
