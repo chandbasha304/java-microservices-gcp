be837c088eca710c7e389ed4df7a2ab967e8c876
Java Developer <developer@example.com>
2026-08-06 17:43:05 +0530

    Expose /actuator/prometheus endpoints and permit Spring Security access for auth-service and order-service

--- auth-service/src/main/java/com/auth/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/auth/auth_service/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**")
+                        .requestMatchers("/auth/**", "/actuator/**")
                         .permitAll()
                         .requestMatchers("/test/**")
                         .hasRole("ADMIN")
